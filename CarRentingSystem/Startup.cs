8a35951f7347fd362deb03717002a25a9ea66344
Milen Ivanov <milensl92@gmail.com>
2021-07-11 16:45:36 +0300

    Implemented - Adding Cars

--- CarRentingSystem/Startup.cs
+++ CarRentingSystem/Startup.cs
@@ -1,47 +1,50 @@
 namespace CarRentingSystem
 {
-using CarRentingSystem.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
+    using CarRentingSystem.Data;
+    using CarRentingSystem.Infrastructure;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
 
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        => Configuration = configuration;
-        
+        => this.Configuration = configuration;
+
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services
-                .AddDbContext<ApplicationDbContext>(options => options
-                .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                .AddDbContext<CarRentingDbContext>(options => options
+                .UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
             services
                 .AddDatabaseDeveloperPageExceptionFilter();
 
             services
-                .AddDefaultIdentity<IdentityUser>(options => 
-                { 
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
                     options.Password.RequireDigit = false;
                     options.Password.RequireLowercase = false;
                     options.Password.RequireNonAlphanumeric = false;
                     options.Password.RequireDigit = false;
                     options.Password.RequireUppercase = false;
-                 })
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                })
+                .AddEntityFrameworkStores<CarRentingDbContext>();
             services
                 .AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.PrepareDatabase();
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -49,7 +52,7 @@ using Microsoft.Extensions.Hosting;
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");   
+                app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
 
@@ -64,6 +67,8 @@ using Microsoft.Extensions.Hosting;
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
             });
+
+
         }
     }
 }
