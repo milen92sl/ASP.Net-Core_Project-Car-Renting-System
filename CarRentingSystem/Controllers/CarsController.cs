5390ce03a540fc9c7b129c9a708012a44a553b1d
Milen Ivanov <milensl92@gmail.com>
2021-07-25 18:02:41 +0300

    Added some more business logic and Services

---
+++ CarRentingSystem/Controllers/CarsController.cs
@@ -0,0 +1,116 @@
+﻿namespace CarRentingSystem.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using CarRentingSystem.Data;
+    using CarRentingSystem.Data.Models;
+    using CarRentingSystem.Infrastructure;
+    using CarRentingSystem.Models.Cars;
+    using CarRentingSystem.Services.Cars;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CarsController : Controller
+    {
+        private readonly ICarService cars;
+        private readonly CarRentingDbContext data;
+
+        public CarsController(CarRentingDbContext data, ICarService cars)
+        {
+            this.data = data;
+            this.cars = cars;
+        }
+
+
+        public IActionResult All([FromQuery] AllCarsQueryModel query)
+        {
+            var queryResult = this.cars.All(
+                           query.Brand,
+                           query.SearchTerm,
+                           query.Sorting,
+                           query.CurrentPage,
+                           AllCarsQueryModel.CarsPerPage);
+
+            var carBrands = this.cars.AllCarBrands();
+
+            query.TotalCars = queryResult.TotalCars;
+            query.Brands = carBrands;
+            query.Cars = queryResult.Cars;
+
+            return View(query);
+        }
+
+        [Authorize]
+        public IActionResult Add()
+        {
+            if (!this.UserIsDealer())
+            {
+                return RedirectToAction(nameof(DealersController.Become), "Dealers");
+            }
+
+            return View(new AddCarFormModel
+            {
+                Categories = this.GetCarCategories()
+            });
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Add(AddCarFormModel car)
+        {
+            var dealerId = this.data
+                .Dealers
+                .Where(d => d.UserId == this.User.GetId())
+                .Select(d => d.Id)
+                .FirstOrDefault();
+
+            if (dealerId == 0)
+            {
+                return RedirectToAction(nameof(DealersController.Become), "Dealers");
+            }
+
+            if (!this.data.Categories.Any(c => c.Id == car.CategoryId))
+            {
+                this.ModelState.AddModelError(nameof(car.CategoryId), "Category does not exist.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                car.Categories = this.GetCarCategories();
+
+                return View(car);
+            }
+
+            var carData = new Car
+            {
+                Brand = car.Brand,
+                Model = car.Model,
+                Description = car.Description,
+                ImageUrl = car.ImageUrl,
+                Year = car.Year,
+                CategoryId = car.CategoryId,
+                DealerId = dealerId
+            };
+
+            this.data.Cars.Add(carData);
+            this.data.SaveChanges();
+
+            return RedirectToAction(nameof(All));
+        }
+
+        private bool UserIsDealer()
+            => this.data
+                .Dealers
+                .Any(d => d.UserId == this.User.GetId());
+
+        private IEnumerable<CarCategoryViewModel> GetCarCategories()
+            => this.data
+                .Categories
+                .Select(c => new CarCategoryViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+                .ToList();
+    }
+}
---
+++ CarRentingSystem/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿namespace CarRentingSystem.Controllers
+{
+    using System.Diagnostics;
+    using System.Linq;
+    using CarRentingSystem.Data;
+    using CarRentingSystem.Models;
+    using CarRentingSystem.Models.Home;
+    using CarRentingSystem.Services.Statistics;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        private readonly IStatisticsService statistics;
+        private readonly CarRentingDbContext data;
+
+        public HomeController(
+            IStatisticsService statistics,
+            CarRentingDbContext data)
+        {
+            this.statistics = statistics;
+            this.data = data;
+        }
+
+        public IActionResult Index()
+        {
+            var totalCars = this.data.Cars.Count();
+            var totalUsers = this.data.Users.Count();
+
+            var cars = this.data
+                .Cars
+                .OrderByDescending(c => c.Id)
+                .Select(c => new CarIndexViewModel
+                {
+                    Id = c.Id,
+                    Brand = c.Brand,
+                    Model = c.Model,
+                    Year = c.Year,
+                    ImageUrl = c.ImageUrl
+                })
+                .Take(3)
+                .ToList();
+
+            var totalStatistics = this.statistics.Total();
+
+            return View(new IndexViewModel
+            {
+                TotalCars = totalStatistics.TotalCars,
+                TotalUsers = totalStatistics.TotalUsers,
+                Cars = cars
+            });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
